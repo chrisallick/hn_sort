91b0fa0554a4ff3cd3f6533c3bb344816a1f65d6
Chris Allick <chrisallick@gmail.com>
2015-04-30 23:26:28 -0400

    adding image

--- ChromeExtension_HN/main.js
+++ ChromeExtension_HN/main.js
@@ -1,43 +1,28 @@
 $(document).ready(function() {
+	$("#hnmain .pagetop:eq(0)").append(" | ");
+
 	var el = document.createElement('a');
 	el.id = "sort_by_comments";
 	el.href = "#";
 	$(el).css({
-		// "z-index": 10000000000000,
-		// "border": "1px solid white",
-		// "background-color": "#000000",
-		// "padding": "10px",
-		// "color": "#FFFFFF",
-		// "position": "absolute",
-		// "top": "80px",
-		// "left": "20px"
-	}).html(" | _comments_").appendTo("#hnmain .pagetop:eq(0)");
+		"color": "#FFFFFF",
+	}).html("_comments_").appendTo("#hnmain .pagetop:eq(0)");
+
+	$("#hnmain .pagetop:eq(0)").append(" | ");
 
 	var el = document.createElement('a');
 	el.id = "sort_by_domain";
 	el.href = "#";
 	$(el).css({
-		// "z-index": 10000000000000,
-		// "border": "1px solid white",
-		// "background-color": "#000000",
-		// "padding": "10px",
-		// "color": "#FFFFFF",
-		// "position": "absolute",
-		// "top": "80px",
-		// "left": "20px"
-	}).html(" | _domain_").appendTo("#hnmain .pagetop:eq(0)");
+		"color": "#FFFFFF",
+	}).html("_domain_").appendTo("#hnmain .pagetop:eq(0)");
+
+	$("#hnmain .pagetop:eq(0)").append(" | ");
 
 	var el = document.createElement('a');
 	el.id = "sort_by_headline";
 	el.href = "#";
 	$(el).css({
-		// "z-index": 10000000000000,
-		// "border": "1px solid white",
-		// "background-color": "#000000",
-		// "padding": "10px",
-		// "color": "#FFFFFF",
-		// "position": "absolute",
-		// "top": "80px",
-		// "left": "20px"
-	}).html(" | _headline-length_").appendTo("#hnmain .pagetop:eq(0)");
+		"color": "#FFFFFF",
+	}).html("_headline-length_").appendTo("#hnmain .pagetop:eq(0)");
 });
